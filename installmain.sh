ab13ae5079200acb027c4a6f162ac421833d9bd5
VALIDEXIS <validexisglobal@gmail.com>
2025-06-12 23:43:23 +0300

    Update installmain.sh

--- installmain.sh
+++ installmain.sh
@@ -25,14 +25,14 @@ sudo apt update
 sudo apt install -y curl git build-essential lz4 wget
 
 sudo rm -rf /usr/local/go
-curl -Ls https://go.dev/dl/go1.23.6.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+curl -Ls https://go.dev/dl/go1.23.8.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
 eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
 eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
 echo "export PATH=$PATH:/usr/local/go/bin:/usr/local/bin:$HOME/go/bin" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
 cd $HOME
-wget -O $HOME/zetacored https://github.com/zeta-chain/node/releases/download/v29.1.2/zetacored-linux-amd64
+wget -O $HOME/zetacored https://github.com/zeta-chain/node/releases/download/v31.0.1/zetacored-linux-amd64
 chmod +x $HOME/zetacored 
 mv $HOME/zetacored $HOME/go/bin
 
